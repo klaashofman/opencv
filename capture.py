ad23b6c2c5d1cefb1d04323120f8d83d43f847aa
klaas <klaas.hofman@gmail.com>
2024-03-09 17:08:11 +0100

    Add image resizing and adaptive thresholding

--- capture.py
+++ capture.py
@@ -1,6 +1,6 @@
 # import the opencv library 
 import cv2 
-
+import numpy as np
 
 # define a video capture object 
 vid = cv2.VideoCapture(0) 
@@ -10,10 +10,22 @@ while(True):
 	# Capture the video frame 
 	# by frame 
 	ret, frame = vid.read() 
+	frame = cv2.resize(frame, (540, 380), fx = 0, fy = 0,
+                         interpolation = cv2.INTER_CUBIC)
 
 	# Display the resulting frame 
 	cv2.imshow('frame', frame) 
 	
+	# conversion of BGR to grayscale is necessary to apply this operation
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+ 
+    # adaptive thresholding to use different threshold 
+    # values on different regions of the frame.
+	Thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                                           cv2.THRESH_BINARY_INV, 11, 2)
+
+
+	cv2.imshow('Tresh', Thresh) 
 	# the 'q' button is set as the 
 	# quitting button you may use any 
 	# desired button of your choice 
